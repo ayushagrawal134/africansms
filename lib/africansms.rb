b9e586f7535315f72129a59251acfb77417d6a3f
Ayush Agrawal <ayagrawal@gammastack.com>
2021-07-15 14:26:03 +0530

    Add configuration

--- lib/africansms.rb
+++ lib/africansms.rb
@@ -1,3 +1,19 @@
+require 'africansms/configuration'
+
 module Africansms
   class AfricansmsError < StandardError; end
+  
+  class << self
+    def configure
+      yield configuration
+    end
+
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def reset
+      @configuration = Configuration.new
+    end
+  end
 end
---
+++ lib/africansms/configuration.rb
@@ -0,0 +1,23 @@
+module Africansms
+  class Configuration
+    attr_accessor :api_key, :username, :shortcode
+
+    def initialize
+      @api_key = nil
+      @username = nil
+      @shortcode = nil
+    end
+
+    def api_key!
+      api_key || raise(AfricansmsError, 'No api key specified.')
+    end
+
+    def username!
+      username || raise(AfricansmsError, 'No username specified.')
+    end
+    
+    def shortcode!
+      shortcode || raise(AfricansmsError, 'No shortcode specified.')
+    end
+  end
+end
